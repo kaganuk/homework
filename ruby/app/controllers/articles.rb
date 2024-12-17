e8d5b8b35faf794d793aea36277173b92e01da5f
kaganuk <kagankilicli@gmail.com>
2024-12-17 22:41:36 +0100

    Return article from update_article Check if it's exits or not

--- ruby/app/controllers/articles.rb
+++ ruby/app/controllers/articles.rb
@@ -1,13 +1,13 @@
 class ArticleController
   def create_article(article)
-    article_not_exists = ! (Article.where(:title => article['title']).empty?)
+    article_not_exists = ! (Article.where(:title => article['title']).exists?)
 
     return { ok: false, msg: 'Article with given title already exists' } unless article_not_exists
 
     new_article = Article.new(:title => article['title'], :content => article['content'], :created_at => Time.now)
     new_article.save
 
-    { ok: false, obj: article }
+    { ok: true, obj: article }
   rescue StandardError
     { ok: false }
   end
@@ -16,13 +16,13 @@ class ArticleController
 
     article = Article.where(id: id).first
 
-    return { ok: false, msg: 'Article could not be found' } unless article.nil?
+    return { ok: false, msg: 'Article could not be found' } unless article.present?
 
     article.title = new_data['title']
     article.content = new_data['content']
-    article.save_changes
+    article.save
 
-    { ok: true }
+    { ok: true, obj: article }
   rescue StandardError
     { ok: false }
   end
